55b5cc7907f713ee5d08573339ddb881492013bd
Liphen <xi.lifeng@qq.com>
2023-12-01 13:44:47 +0800

    fix(ExeAccessMem): 例外信号逻辑错误

--- chisel/playground/src/pipeline/execute/ExeAccessMemCtrl.scala
+++ chisel/playground/src/pipeline/execute/ExeAccessMemCtrl.scala
@@ -76,8 +76,8 @@ class ExeAccessMemCtrl(implicit val config: CpuConfig) extends Module {
   for (i <- 0 until config.fuNum) {
     val store_inst = LSUOpType.isStore(io.inst(i).info.op)
     io.inst(i).ex.out                                := io.inst(i).ex.in
-    io.inst(i).ex.out.exception(loadAddrMisaligned)  := store_inst && !addr_aligned(i)
-    io.inst(i).ex.out.exception(storeAddrMisaligned) := !store_inst && !addr_aligned(i)
+    io.inst(i).ex.out.exception(loadAddrMisaligned)  := !store_inst && !addr_aligned(i)
+    io.inst(i).ex.out.exception(storeAddrMisaligned) := store_inst && !addr_aligned(i)
   }
   io.inst(0).mem_sel := (io.inst(0).info.fusel === FuType.lsu) &&
     !(io.inst(0).ex.out.exception.asUInt.orR || io.inst(0).ex.out.interrupt.asUInt.orR) &&
