3134b79c5416e96730d992aa6f4f1947f96afe6f
Liphen <xi.lifeng@qq.com>
2023-12-19 15:08:24 +0800

    增加puamips的cache文件

--- chisel/playground/src/CpuConfig.scala
+++ chisel/playground/src/CpuConfig.scala
@@ -26,3 +26,29 @@ case class CpuConfig(
 case class BranchPredictorConfig(
   val bhtDepth: Int = 5,
   val phtDepth: Int = 6)
+
+case class CacheConfig(
+  nway:      Int = 2, // 路数
+  nbank:     Int = 8, // bank数
+  nset:      Int,
+  bankWidth: Int // bytes per bank
+) {
+  val config          = CpuConfig()
+  val indexWidth      = log2Ceil(nset) // 6
+  val bankIndexWidth  = log2Ceil(nbank) // 3
+  val bankOffsetWidth = log2Ceil(bankWidth) // 3
+  val offsetWidth     = bankIndexWidth + bankOffsetWidth // 6
+  val tagWidth        = 32 - indexWidth - offsetWidth // 20
+  val tagvWidth       = tagWidth + 1 // 21
+  val bankWidthBits   = bankWidth * 8 // 64
+  val burstSize       = 16
+  val ninst           = config.instFetchNum // TODO:改成可随意修改的参数
+  require(isPow2(nset))
+  require(isPow2(nway))
+  require(isPow2(nbank))
+  require(isPow2(bankWidth))
+  require(
+    tagWidth + indexWidth + bankIndexWidth + bankOffsetWidth == 32,
+    "basic request calculation"
+  )
+}
