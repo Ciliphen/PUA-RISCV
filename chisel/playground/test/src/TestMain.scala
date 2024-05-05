1cf0bee889f4c1678ae36c26b63bbdf620432560
Liphen <xi.lifeng@qq.com>
2024-05-05 18:57:13 +0800

    更新submodule

--- chisel/playground/test/src/TestMain.scala
+++ chisel/playground/test/src/TestMain.scala
@@ -1,18 +1,15 @@
-// import cpu._
-// import circt.stage._
+import cpu._
+import circt.stage._
 
-// import cpu.pipeline.Csr
-// import icache.mmu.Tlb
+import cpu.pipeline.Csr
+import icache.mmu.Tlb
+import cpu.pipeline.ARegFile
+import cpu.pipeline.Alu
 
-// object TestMain extends App {
-//   implicit val cpuConfig       = new CpuConfig()
-//   implicit val dCacheConfig = CacheConfig(cacheType = "dcache")
-//   def top                   = new DCache(dCacheConfig)
-//   val useMFC                = false // use MLIR-based firrtl compiler
-//   val generator             = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-//   if (useMFC) {
-//     (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-//   } else {
-//     (new chisel3.stage.ChiselStage).execute(args, generator)
-//   }
-// }
+object TestMain extends App {
+  implicit val cpuConfig    = new CpuConfig()
+  implicit val dCacheConfig = CacheConfig(cacheType = "dcache")
+  def top                   = new Alu()
+  val generator             = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
+    (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+}
